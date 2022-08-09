ab63f8ae3d95c3ad5f3a913ab1ad121a87d14470
Deyao Chen <chendeyao000@gmail.com>
2022-08-09 15:03:53 +0800

    add rest api

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 
 from django.contrib import admin
 
+from rest_framework import routers
+
 admin.autodiscover()
 
 import hello.views
+import restapi.views
 
 # To add a new path, first import the app:
 # import blog
@@ -14,8 +17,16 @@ import hello.views
 #
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
+
+rest_router = routers.DefaultRouter()
+rest_router.register(r'users', restapi.views.UserViewSet)
+rest_router.register(r'groups', restapi.views.GroupViewSet)
+
+
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path("api/", include(rest_router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
